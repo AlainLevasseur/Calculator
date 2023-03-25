ee18515787bbf7c8fbca09d40a1b5be9c89a485d
Alain Levasseur <118285760+AlainLevasseur@users.noreply.github.com>
2023-03-25 17:24:23 -0400

    Add functionality to number buttons

--- calculator.js
+++ calculator.js
@@ -1,6 +1,12 @@
 let registerA = 10;
 let registerB = 5;
 
+let displayValue = 0;
+let displayScreen = document.getElementById('display');
+
+let clearButton = document.getElementById('clear');
+let operation = '';
+
 function add(augend, addend) {
     return augend + addend;
 }
@@ -17,6 +23,26 @@ function divide(dividend, divisor) {
     return dividend / divisor;
 }
 
+//Add functionality to the numbered buttons
+var button;
+for(let i = 0; i < 10; i++){
+    button = document.querySelector(`.num-${i}`);
+    button.addEventListener("click", function() {
+        displayValue += `${i}`;
+        displayScreen.innerText = displayValue;
+    });
+}
+
+//Add functionality to the clear button
+clearButton.addEventListener("click", function() {
+    displayValue = '';
+    displayScreen.innerText = '';
+});
+
+function pressOperator(op) {
+
+}
+
 function operate(operator, num1, num2) {
     switch(operator){
         case '+': return add(num1, num2);
@@ -26,8 +52,3 @@ function operate(operator, num1, num2) {
         default: return 'INVALID';
     }
 }
-
-console.log(`add: ${operate('+', registerA, registerB)}, 
-            subtract: ${operate('-', registerA, registerB)}, 
-            multiply: ${operate('*', registerA, registerB)}, 
-            divide: ${operate('/', registerA, registerB)}`);
